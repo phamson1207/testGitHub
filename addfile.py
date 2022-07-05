8120b72fa756d85a1be0b271ee06cbd2a1d0690a
phamson1207 <cothanvanlydu@gmail.com>
2022-07-05 12:55:15 +0700

    update lan3

---
+++ addfile.py
@@ -0,0 +1,6 @@
+from multiprocessing.connection import wait
+import cv2
+from cv2 import waitKey
+anh = cv2.imread("anhMau.png")
+cv2.imshow("",anh)
+cv2,waitKey()
--- batVatThe.py
+++ batVatThe.py
@@ -1,5 +1,16 @@
 import cv2
 from numpy import imag
-imgae = cv2.imread("round.png")
+imgae = cv2.imread("anhMau.png")
+imgaeHSV =imgae
+frameReSize = cv2.resize(imgaeHSV,dsize=(400,400) ) # co dinh kich thuoc khung hinh
 cv2.imshow("",imgae)
+for i in range(400):
+    for j in range (400):
+        temp = frameReSize[i][j] 
+        temp[0] = 0
+        temp[2] = 0
+        frameReSize[i][j] = temp
+
+cv2.imshow("",frameReSize)
 cv2.waitKey()
+cv2.destroyAllWindows()
